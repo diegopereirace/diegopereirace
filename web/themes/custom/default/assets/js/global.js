f45f3658bcccf3d2ebaf61df9455272b18a0ff19
Diego Pereira <151082544+diegopereirace@users.noreply.github.com>
2025-05-03 13:00:55 -0300

    update

---
+++ web/themes/custom/default/assets/js/global.js
@@ -0,0 +1,11 @@
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.default = {
+    attach: function (context, settings) {
+      console.log('Drupal behaviors are working!');
+    }
+  };
+
+})(Drupal);
