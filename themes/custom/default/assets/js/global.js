aa234f0db81b605a03fe4b2d08558af18d34c853
Diego Pereira <151082544+diegopereirace@users.noreply.github.com>
2025-07-26 20:59:12 -0300

    coisas de tema

--- themes/custom/default/assets/js/global.js
+++ themes/custom/default/assets/js/global.js
@@ -7,9 +7,21 @@
 
   'use strict';
 
-  Drupal.behaviors.bootstrap_barrio_subtheme = {
+  Drupal.behaviors.default = {
     attach: function (context, settings) {
-
+      new Scene({
+        ".square": i => ({
+          opacity: [0, 1],
+          options: {
+            duration: 1,
+            delay: (i % 7) * 0.1 + Math.floor(i / 7) * 0.2,
+          }
+        }),
+      }, {
+        selector: true,
+        direction: "alternate",
+        iterationCount: "infinite",
+      }).play();      
     }
   };
 
