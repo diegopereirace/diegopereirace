9a1d58fd570cdc0526f966ba7b5b7a91b1e5bdec
Diego Pereira <151082544+diegopereirace@users.noreply.github.com>
2025-09-22 13:20:19 -0300

    Create base.js

---
+++ themes/contrib/olivero/js/base.js
@@ -0,0 +1,11 @@
+(function ($, Drupal) {
+
+    'use strict';
+
+    Drupal.behaviors.base = {
+        attach: function (context, settings) {
+            
+        }
+    };
+
+})(jQuery, Drupal);
