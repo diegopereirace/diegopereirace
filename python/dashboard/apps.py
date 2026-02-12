be0fdae15ccb4c1827a19f6195ebd281cfa9cd13
Diego Pereira <151082544+diegopereirace@users.noreply.github.com>
2026-02-12 13:01:19 -0300

    exclui /python

--- python/dashboard/apps.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.apps import AppConfig
-
-
-class DashboardConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'dashboard'
-    verbose_name = 'Dashboard de Vendas'
