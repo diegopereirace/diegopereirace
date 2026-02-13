593a27da37d4fd19e01c9099522018d6d9cee600
Diego Pereira <151082544+diegopereirace@users.noreply.github.com>
2026-02-13 10:22:46 -0300

    Update index.php

--- index.php
+++ index.php
@@ -111,7 +111,7 @@ $navLinks = [
     ['name' => 'Home', 'href' => '#home'],
     ['name' => 'Sobre', 'href' => '#about'],
     ['name' => 'Skills', 'href' => '#skills'],
-    ['name' => 'Exercícios em Python · Análise de Dados', 'href' => 'https://portfolio-py-c45uvaal8fvasrmvlxrcxs.streamlit.app/'],
+    ['name' => 'Exercícios em Python · Análise de Dados', 'href' => 'https://portfolio-py.diegopereirace.com.br'],
     ['name' => 'CONTRATE-ME', 'href' => '#contact']
 ];
 
